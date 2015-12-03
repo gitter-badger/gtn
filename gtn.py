d85b56346aaadfa12549d3ae57c1c7ff8f5130a6
Val Saven <val.saven@gmail.com>
2015-12-03 23:47:28 +0500

    Added a few minor improvements
    + New welcome screen.
    + Template for difficulty levels which will appear in the future.
    + The game goes to the victorious end (I'll add difficulty levels in the future).
    fix: New loop statements "while" instead "if".

--- gtn.py
+++ gtn.py
@@ -11,28 +11,32 @@ Hard = 1
 import random
 
 print """\
-*********************************
-* Guess the number welcomes you!*
-*                               *
-*      You have 3 attempts!     *
-*                               *
-*          Let's go!            *
-*********************************
+************************************
+* "Guess the number" welcomes you! *
+*                                  *
+*        Difficulty Levels:        * 
+*                                  *
+*  1 - Easy (you have 5 attempt)   *
+*  2 - Normal (you have 3 attempt) *
+*  3 - Hard (you have 1 attempt)   *
+*                                  *
+*             Let's go!            *
+************************************
 """
+print ("Select Difficulty Level:")
+dif = input()
 
 num = random.randrange(1,10)
+x = 0
 
-print ("Enter your number:")
-x = input()
+while num != x:
+    print ("Enter your number:")
+    x = input()
+    if num > x:
+        print ("It's bigger!\n")
+    if num < x:
+        print ("It's smaller!\n")
+    pass
 
-if num > x:
-    print ("It's bigger!")
-    print num
-
-if num < x:
-    print ("It's smaller!")
-    print num
-
-if num == x:
-    print ("You win!")
-    print num
+print ("You win!")
+print num
