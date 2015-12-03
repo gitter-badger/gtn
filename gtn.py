4f029d9525975848de5c148f6b27030f6db6f18c
Val Saven <val.saven@gmail.com>
2015-12-03 23:37:06 +0500

    Added a few minor improvements
    ~ Put down the "num==x" condition
    ~ "Welcome..."-text now occupies one line

--- gtn.py
+++ gtn.py
@@ -2,26 +2,28 @@
 Guess the number!
 Generate X
 + or - while user doesn't win
+You can select number of attempts
+Easy = 5
+Medium = 3
+Hard = 1
 """
 
 import random
 
-print ("'Guess the number' welcomes you!")
-print ("You have 3 attempts!")
-print ("Let's go!")
+print ("\"Guess the number\" welcomes you!\nYou have 3 attempts!\nLet's go!")
 num = random.randrange(1,10)
 
 print ("Enter your number:")
 x = input()
 
-if num == x:
-    print ("You win!")
-    print num
-
 if num > x:
-    print ("It's bigger")
+    print ("It's bigger!")
     print num
 
 if num < x:
-    print ("It's smaller")
+    print ("It's smaller!")
+    print num
+
+if num == x:
+    print ("You win!")
     print num
