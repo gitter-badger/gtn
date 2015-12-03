3ef3f6dda8c2c8d0631cd959cbdc0557de5a2c0b
Val Saven <val.saven@gmail.com>
2015-12-03 23:58:43 +0500

    Added a few minor improvements
    Added two functions.
    Added an opportunity to restart after win.

--- gtn.py
+++ gtn.py
@@ -9,34 +9,47 @@ Hard = 1
 """
 
 import random
+import sys
 
-print """\
-************************************
-* "Guess the number" welcomes you! *
-*                                  *
-*        Difficulty Levels:        * 
-*                                  *
-*  1 - Easy (you have 5 attempt)   *
-*  2 - Normal (you have 3 attempt) *
-*  3 - Hard (you have 1 attempt)   *
-*                                  *
-*             Let's go!            *
-************************************
-"""
-print ("Select Difficulty Level:")
-dif = input()
+def play():
+    print """\
+    ************************************
+    * "Guess the number" welcomes you! *
+    *                                  *
+    *        Difficulty Levels:        * 
+    *                                  *
+    *  1 - Easy (you have 5 attempt)   *
+    *  2 - Normal (you have 3 attempt) *
+    *  3 - Hard (you have 1 attempt)   *
+    *                                  *
+    *             Let's go!            *
+    ************************************
+    """
+#   print ("Select Difficulty Level:")
+#   dif = input()
+    
+    num = random.randrange(1,10)
+    x = 0
+    
+    while num != x:
+        print ("Enter your number:")
+        x = input()
+        if num > x:
+            print ("It's bigger!\n")
+        if num < x:
+            print ("It's smaller!\n")
+        pass
+    
+    print ("You win!")
+    print num
 
-num = random.randrange(1,10)
-x = 0
-
-while num != x:
-    print ("Enter your number:")
-    x = input()
-    if num > x:
-        print ("It's bigger!\n")
-    if num < x:
-        print ("It's smaller!\n")
-    pass
-
-print ("You win!")
-print num
+def main():
+    play()
+    while True:
+        restart = input("Would you like to play again? Please enter '1' for YES or '2' for NO: ")
+        if restart==1:
+            play()
+        else:
+            print "Thanks for playing!"
+            break
+main()
